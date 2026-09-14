9a81a9d7659053d8a866a4c994a6dbe3667148e2
deepakmishra9617 <Deepakmishra9617@users.noreply.github.com.>
2026-09-14 20:14:21 +0530

    Initial commit

---
+++ resume_frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='p-10'>
+        <h1 className="text-5xl text-gray-700 font-bold">
+          Welcome to the Resume Generator
+        </h1>
+
+        
+        <button className="btn btn-primary mt-5">Test DaisyUI</button>
+      </div>
+    </>
+  )
+}
+
+export default App
